91c8b3bb180cd61262fb52fe886f0664cf98a5b0
unknown <almas@oomzie.com>
2019-02-10 15:21:08 -0500

    added hero icons

--- src/heroes.js
+++ src/heroes.js
@@ -1,335 +1,334 @@
 export const heroes = [
   {
-    name: 'Abbadon',
+    name: 'Abaddon',
     undead: true,
     knight: true,
-    portrait: ''
+
   },
   {
     name: 'Alchemist',
     goblin: true,
     warlock: true,
-    portrait: ''
   },
   {
     name: 'Anti Mage',
     elf: true,
     demonhunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Axe',
     orc: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Bat Rider',
     troll: true,
     kight: true,
-    portrait: ''
+ 
   },
   {
     name: 'Beast Master',
     orc: true,
     hunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Bounty Hunter',
     goblin: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Chaos Knight',
     demon: true,
     kight: true,
-    portrait: ''
+ 
   },
   {
     name: 'Clockwerk',
     goblin: true,
     mech: true,
-    portrait: ''
+ 
   },
   {
     name: 'Crystal Maiden',
     human: true,
     mage: true,
-    portrait: ''
+ 
   },
   {
     name: 'Disruptor',
     orc: true,
     shaman: true,
-    portrait: ''
+ 
   },
   {
     name: 'Doom',
     demon: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Dragon Knight',
     human: true,
     dragon: true,
     kight: true,
-    portrait: ''
+ 
   },
   {
     name: 'Drow Ranger',
     undead: true,
     hunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Enchantress',
     beast: true,
     druid: true,
-    portrait: ''
+ 
   },
   {
     name: 'Enigma',
     elemental: true,
     warlock: true,
-    portrait: ''
+ 
   },
   {
     name: 'Furion',
     elf: true,
     druid: true,
-    portrait: ''
+ 
   },
   {
     name: 'Gyrocopter',
     dwarf: true,
     mech: true,
-    portrait: ''
+ 
   },
   {
     name: 'Juggernaut',
     ogre: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Keeper of the Light',
     human: true,
     mage: true,
-    portrait: ''
+ 
   },
   {
     name: 'Kunkka',
     human: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Lich',
     undead: true,
     mage: true,
-    portrait: ''
+ 
   },
   {
     name: 'Lina',
     human: true,
     mage: true,
-    portrait: ''
+ 
   },
   {
     name: 'Lone Druid',
     beast: true,
     druid: true,
-    portrait: ''
+ 
   },
   {
     name: 'Luna',
     elf: true,
     kight: true,
-    portrait: ''
+ 
   },
   {
     name: 'Lycan',
     human: true,
     beast: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Medusa',
     naga: true,
     hunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Morphling',
     elemental: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Necrophos',
     undead: true,
     warlock: true,
-    portrait: ''
+ 
   },
   {
     name: 'Ogre Magi',
     ogre: true,
     mage: true,
-    portrait: ''
+ 
   },
   {
     name: 'Omni Knight',
     human: true,
     kight: true,
-    portrait: ''
+ 
   },
   {
     name: 'Phantom Assassin',
     elf: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Puck',
     elf: true,
     dragon: true,
     mage: true,
-    portrait: ''
+ 
   },
   {
     name: 'Queen of Pain',
     demon: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Razor',
     elemental: true,
     mage: true,
-    portrait: ''
+ 
   },
   {
     name: 'Sand King',
     beast: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Shadow Fiend',
     demon: true,
     warlock: true,
-    portrait: ''
+ 
   },
   {
     name: 'Shadow Shaman',
     troll: true,
     shaman: true,
-    portrait: ''
+ 
   },
   {
     name: 'Slardar',
     naga: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Slark',
     naga: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Sniper',
     dwarf: true,
     hunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Techies',
     goblin: true,
     mech: true,
-    portrait: ''
+ 
   },
   {
     name: 'Templar Assassin',
     elf: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Terrorblade',
     demon: true,
     demonhunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Tidehunter',
     naga: true,
     hunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Timbersaw',
     goblin: true,
     mech: true,
-    portrait: ''
+ 
   },
   {
     name: 'Tinker',
     goblin: true,
     mech: true,
-    portrait: ''
+ 
   },
   {
     name: 'Tiny',
     elemental: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Treant Protector',
     elf: true,
     druid: true,
-    portrait: ''
+ 
   },
   {
     name: 'Troll Warlord',
     troll: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Tusk',
     beast: true,
     warrior: true,
-    portrait: ''
+ 
   },
   {
     name: 'Venomancer',
     beast: true,
     warlock: true,
-    portrait: ''
+ 
   },
   {
     name: 'Viper',
     dragon: true,
     assasin: true,
-    portrait: ''
+ 
   },
   {
     name: 'Wind Ranger',
     elf: true,
     hunter: true,
-    portrait: ''
+ 
   },
   {
     name: 'Witch Doctor',
     troll: true,
     shaman: true,
-    portrait: ''
+ 
   }
 ]
