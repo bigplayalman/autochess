59a12a5ddac20b80a208420a0b571b8dea6db8e2
unknown <almas@oomzie.com>
2019-02-12 22:23:04 -0500

    adde synergies

--- src/classes.js
+++
@@ -1,7 +0,0 @@
-const classes = [
-    {
-        type: 'assasin',
-        count: 3,
-        effect: 'All friendly assassins have a 10% chance to deal 3.5x damage'
-    }
-]
---
+++ src/synergies.js
@@ -0,0 +1,227 @@
+export const synergies = [
+    {
+        type: 'assasin',
+        count: 3,
+        effect: 'All friendly assassins have a 10% chance to deal 3.5x damage'
+    },
+    {
+        type: 'assasin',
+        count: 6,
+        effect: 'All friendly assassins have an additional 20% chance to deal 4.5x damage'
+    },
+    {
+        type: 'demonhunter',
+        count: 1,
+        effect: 'Negates an enemy demon’s power.'
+    },
+    {
+        type: 'demonhunter',
+        count: 2,
+        effect: 'All friendly demons keep their power.'
+    },
+    {
+        type: 'druid',
+        count: 2,
+        effect: 'Two one-star druids can upgrade to a two-stars druid.'
+    },
+    {
+        type: 'druid',
+        count: 4,
+        effect: 'Two two-stars druids can upgrade to a three-stars druid.'
+    },
+    {
+        type: 'hunter',
+        count: 3,
+        effect: 'Attack damage increased by 25% for all friendly hunters.'
+    },
+    {
+        type: 'hunter',
+        count: 6,
+        effect: 'Attack damage increased by an additional 35% for all friendly hunters.'
+    },
+    {
+        type: 'knight',
+        count: 2,
+        effect: 'All friendly knights have a 25% chance to trigger a damage-reduction shield when attacked.'
+    },
+    {
+        type: 'knight',
+        count: 4,
+        effect: 'All friendly knights have a 35% chance to trigger a damage-reduction shield when attacked.'
+    },
+    {
+        type: 'knight',
+        count: 6,
+        effect: 'All friendly knights have a 45% chance to trigger a damage-reduction shield when attacked.'
+    },
+    {
+        type: 'mage',
+        count: 3,
+        effect: 'Magic resistance decreased by 40% for all enemies.'
+    },
+    {
+        type: 'mage',
+        count: 6,
+        effect: 'Magic resistance decreased by an additional 40% for all enemies.'
+    },
+    {
+        type: 'mech',
+        count: 2,
+        effect: 'HP regeneration increased by 15 for all friendly mechs.'
+    },
+    {
+        type: 'mech',
+        count: 4,
+        effect: 'HP regeneration increased by an additional 25 for all friendly mechs.'
+    },
+    {
+        type: 'shaman',
+        count: 2,
+        effect: 'Hex affects a random enemy when the battle starts.'
+    },
+    {
+        type: 'warlock',
+        count: 3,
+        effect: 'Provides 20% lifesteal to all allies.'
+    },
+    {
+        type: 'warlock',
+        count: 6,
+        effect: 'Provides an additional 20% lifesteal for all allies.'
+    },
+    {
+        type: 'warrior',
+        count: 3,
+        effect: 'Armor increased by 7 for all friendly warriors.'
+    },
+    {
+        type: 'warrior',
+        count: 6,
+        effect: 'Armor increased by an additional 8 for all friendly warriors.'
+    },
+    {
+        type: 'warrior',
+        count: 9,
+        effect: 'Armor increased by and additional 9 for all friendly warriors.'
+    },
+    {
+        type: 'beast',
+        count: 2,
+        effect: 'Attack damage increased by 10% for all allies'
+    },
+    {
+        type: 'beast',
+        count: 4,
+        effect: 'Attack damage increased by an additional 15% for all allies'
+    },
+    {
+        type: 'beast',
+        count: 6,
+        effect: 'Attack damage increased by an additional 20% for all allies'
+    },
+    {
+        type: 'demon',
+        count: 1,
+        effect: 'Deals 50% extra pure damage to their target.'
+    },
+    {
+        type: 'dwarf',
+        count: 1,
+        effect: 'Attack range increased by 300.'
+    },
+    {
+        type: 'dragon',
+        count: 3,
+        effect: 'All friendly dragons have 100 mana when the battle starts.'
+    },
+    {
+        type: 'element',
+        count: 2,
+        effect: 'All friendly elements have a 30% chance to turn the attacker into stone for 3 seconds when attacked by melee chess pieces.'
+    },
+    {
+        type: 'element',
+        count: 4,
+        effect: 'All allies have a 30% chance to turn the attacker into stone for 3 seconds when attacked by melee chess pieces.'
+    },
+    {
+        type: 'elf',
+        count: 3,
+        effect: 'Evasion increased by 25% for all friendly elves.'
+    },
+    {
+        type: 'elf',
+        count: 6,
+        effect: 'Evasion increased by an additional 25% for all friendly elves.'
+    },
+    {
+        type: 'goblin',
+        count: 3,
+        effect: 'Armor and HP regeneration increased by 15 for a random ally.'
+    },
+    {
+        type: 'goblin',
+        count: 6,
+        effect: 'Armor and HP regeneration increased by 15 for all friendly goblins.'
+    },
+    {
+        type: 'human',
+        count: 2,
+        effect: 'All friendly humans have a 20% chance to disarm the target for 3s when attacking.'
+    },
+    {
+        type: 'human',
+        count: 4,
+        effect: 'All friendly humans have an additional 25% chance to disarm the target for 3s when attacking.'
+    },
+    {
+        type: 'human',
+        count: 6,
+        effect: 'All friendly humans have an additional 30% chance to disarm the target for 3s when attacking.'
+    },
+    {
+        type: 'naga',
+        count: 2,
+        effect: 'Magic resistance increased by 20% for all allies.'
+    },
+    {
+        type: 'naga',
+        count: 4,
+        effect: 'Magic resistance increased by an additional 40% for all allies.'
+    },
+    {
+        type: 'ogre',
+        count: 1,
+        effect: 'Max health points increased by 5% for each head.'
+    },
+    {
+        type: 'orc',
+        count: 2,
+        effect: 'Max health points increased by 250 for all friendly orcs.'
+    },
+    {
+        type: 'orc',
+        count: 4,
+        effect: 'Max health points increased by an additional 350 for all friendly orcs.'
+    },
+    {
+        type: 'troll',
+        count: 2,
+        effect: 'Attack speed increased by 30% for all friendly trolls.'
+    },
+    {
+        type: 'troll',
+        count: 4,
+        effect: 'Attack speed increased by 30% for all allies.'
+    },
+    {
+        type: 'undead',
+        count: 2,
+        effect: 'Armor decreased by 5 for all enemies.'
+    },
+    {
+        type: 'undead',
+        count: 4,
+        effect: 'Armor decreased by 7 for all enemies.'
+    },
+]
