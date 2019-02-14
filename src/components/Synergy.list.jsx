c472f211543993adfbce4332d124f4135915f8d5
unknown <almas@oomzie.com>
2019-02-14 00:17:04 -0500

    adjusted styles

--- src/components/Synergy.list.jsx
+++ src/components/Synergy.list.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 
-
-const SynergyList = ({className, synergies, activeSynergies, title}) => {
+const SynergyList = ({ className, synergies, activeSynergies, title }) => {
   return (
     <div className={className}>
-    <strong>{title}</strong>
-      {synergies.map((synergy, index) => {
-        let effectClass = '';
-        if(activeSynergies.filter(x => x.effect === synergy.effect && x.count === synergy.count).length) {
-          effectClass = 'selected'
-        }
-        return (
-          <div className={effectClass} key={`${synergy.type}-${index}-${className}`}>
-            {synergy.effect}
-          </div>
-        );
-      })}
+      <strong>{title}</strong>
+        {synergies.map((synergy, index) => {
+          let effectClass = '';
+          if (activeSynergies.filter(x => x.effect === synergy.effect && x.count === synergy.count).length) {
+            effectClass = 'selected'
+          }
+
+          return (
+            <div className={effectClass} key={`${synergy.type}-${index}-${className}`}>
+              {synergy.count} {synergy.type}
+              <div>{synergy.effect}</div>
+            </div>
+          );
+        })}
+
     </div>
   );
 };
