7e71995ccef7621858b2dd7106ee28e4c7d70c31
Almas Barzhaxynov <almas@oomzie.com>
2019-02-14 17:17:18 -0500

    fixed witch doctor synergy

--- src/heroes.js
+++ src/heroes.js
@@ -14,321 +14,321 @@ export const heroes = [
     name: 'Anti Mage',
     elf: true,
     demonhunter: true,
- 
+
   },
   {
     name: 'Axe',
     orc: true,
     warrior: true,
- 
+
   },
   {
     name: 'Bat Rider',
     troll: true,
     knight: true,
- 
+
   },
   {
     name: 'Beast Master',
     orc: true,
     hunter: true,
- 
+
   },
   {
     name: 'Bounty Hunter',
     goblin: true,
     assasin: true,
- 
+
   },
   {
     name: 'Chaos Knight',
     demon: true,
     knight: true,
- 
+
   },
   {
     name: 'Clockwerk',
     goblin: true,
     mech: true,
- 
+
   },
   {
     name: 'Crystal Maiden',
     human: true,
     mage: true,
- 
+
   },
   {
     name: 'Disruptor',
     orc: true,
     shaman: true,
- 
+
   },
   {
     name: 'Doom',
     demon: true,
     warrior: true,
- 
+
   },
   {
     name: 'Dragon Knight',
     human: true,
     dragon: true,
     knight: true,
- 
+
   },
   {
     name: 'Drow Ranger',
     undead: true,
     hunter: true,
- 
+
   },
   {
     name: 'Enchantress',
     beast: true,
     druid: true,
- 
+
   },
   {
     name: 'Enigma',
     elemental: true,
     warlock: true,
- 
+
   },
   {
     name: 'Furion',
     elf: true,
     druid: true,
- 
+
   },
   {
     name: 'Gyrocopter',
     dwarf: true,
     mech: true,
- 
+
   },
   {
     name: 'Juggernaut',
     ogre: true,
     warrior: true,
- 
+
   },
   {
     name: 'Keeper of the Light',
     human: true,
     mage: true,
- 
+
   },
   {
     name: 'Kunkka',
     human: true,
     warrior: true,
- 
+
   },
   {
     name: 'Lich',
     undead: true,
     mage: true,
- 
+
   },
   {
     name: 'Lina',
     human: true,
     mage: true,
- 
+
   },
   {
     name: 'Lone Druid',
     beast: true,
     druid: true,
- 
+
   },
   {
     name: 'Luna',
     elf: true,
     knight: true,
- 
+
   },
   {
     name: 'Lycan',
     human: true,
     beast: true,
     warrior: true,
- 
+
   },
   {
     name: 'Medusa',
     naga: true,
     hunter: true,
- 
+
   },
   {
     name: 'Morphling',
     elemental: true,
     assasin: true,
- 
+
   },
   {
     name: 'Necrophos',
     undead: true,
     warlock: true,
- 
+
   },
   {
     name: 'Ogre Magi',
     ogre: true,
     mage: true,
- 
+
   },
   {
     name: 'Omni Knight',
     human: true,
     knight: true,
- 
+
   },
   {
     name: 'Phantom Assassin',
     elf: true,
     assasin: true,
- 
+
   },
   {
     name: 'Puck',
     elf: true,
     dragon: true,
     mage: true,
- 
+
   },
   {
     name: 'Queen of Pain',
     demon: true,
     assasin: true,
- 
+
   },
   {
     name: 'Razor',
     elemental: true,
     mage: true,
- 
+
   },
   {
     name: 'Sand King',
     beast: true,
     assasin: true,
- 
+
   },
   {
     name: 'Shadow Fiend',
     demon: true,
     warlock: true,
- 
+
   },
   {
     name: 'Shadow Shaman',
     troll: true,
     shaman: true,
- 
+
   },
   {
     name: 'Slardar',
     naga: true,
     warrior: true,
- 
+
   },
   {
     name: 'Slark',
     naga: true,
     assasin: true,
- 
+
   },
   {
     name: 'Sniper',
     dwarf: true,
     hunter: true,
- 
+
   },
   {
     name: 'Techies',
     goblin: true,
     mech: true,
- 
+
   },
   {
     name: 'Templar Assassin',
     elf: true,
     assasin: true,
- 
+
   },
   {
     name: 'Terrorblade',
     demon: true,
     demonhunter: true,
- 
+
   },
   {
     name: 'Tidehunter',
     naga: true,
     hunter: true,
- 
+
   },
   {
     name: 'Timbersaw',
     goblin: true,
     mech: true,
- 
+
   },
   {
     name: 'Tinker',
     goblin: true,
     mech: true,
- 
+
   },
   {
     name: 'Tiny',
     elemental: true,
     warrior: true,
- 
+
   },
   {
     name: 'Treant Protector',
     elf: true,
     druid: true,
- 
+
   },
   {
     name: 'Troll Warlord',
     troll: true,
     warrior: true,
- 
+
   },
   {
     name: 'Tusk',
     beast: true,
     warrior: true,
- 
+
   },
   {
     name: 'Venomancer',
     beast: true,
     warlock: true,
- 
+
   },
   {
     name: 'Viper',
     dragon: true,
     assasin: true,
- 
+
   },
   {
     name: 'Wind Ranger',
     elf: true,
     hunter: true,
- 
+
   },
   {
     name: 'Witch Doctor',
     troll: true,
-    shaman: true,
- 
+    warlock: true,
+
   }
 ]
