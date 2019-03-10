47fcd58bb0071a1f7e534c66a4744ae0786c48e9
Almas Barzhaxynov <almas@oomzie.com>
2019-03-10 16:45:41 -0400

    updated heroes to show up in the sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import {connect} from "react-redux";
+import { actionCreators as heroStore } from "../store/services/hero.service";
 
-const Header = () => {
-  return (
-    <header>
-      <strong>
-        Dota Auto Chess Synergy
-      </strong>
-    </header>
-  );
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <strong>
+          Dota Auto Chess Synergy
+        </strong>
+        <label>
+          <input type='checkbox' value={this.props.heroes.show} onChange={({ target: { checked } }) => { this.props.toggleHero(checked) }} />
+          <span className='menu'>
+            Heroes
+            <span className='hamburger' />
+          </span>
+        </label>
+      </header>
+    );
+  }
 };
 
-export default Header;
+const getData = (state, store) => state[store];
+
+const mapStateToProps = (state) => {
+  return {
+    heroes: getData(state, 'heroes')
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return ({
+    toggleHero: (hero) => dispatch(heroStore.toggleHero(hero))
+  });
+};
+const HeaderConnected = connect(mapStateToProps, mapDispatchToProps)(Header);
+export default HeaderConnected;
