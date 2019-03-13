2790b734559b31e2c7d33bc16566211e8ee575c4
Almas Barzhaxynov <almas@oomzie.com>
2019-03-13 00:01:42 -0400

    added github link back

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -15,7 +15,12 @@ export const Layout = () => {
         <HeroList />
       </main>
       <footer>
-        Dota 2 content and materials are trademarks and copyrights of Valve or its licensors.  This site is not affiliated with Valve.
+        <div>
+          Dota 2 content and materials are trademarks and copyrights of Valve or its licensors.  This site is not affiliated with Valve.
+        </div>
+        <div>
+          <a href="https://github.com/bigplayalman/autochess" target="_blank" rel="noopener noreferrer">View source code on Github</a>
+        </div>
       </footer>
     </div>
   );
